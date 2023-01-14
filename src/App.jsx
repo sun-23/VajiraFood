4254f50176839f4d467fc935d994d727589fac63
Borwornpob Thumrongchotikhun <borwornpob@gmail.com>
2023-01-14 20:20:34 +0700

    Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import CreateReport from "./routes/CreateReport";
+import ListAllProblems from "./routes/ListAllProblems";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -12,6 +14,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new_rep" element={<CreateReport />} />
+        <Route path="/problems" element={<ListAllProblems />} />
       </Routes>
       <Footer />
     </Router>
--- src/routes/ListAllProblems.jsx
+++ src/routes/ListAllProblems.jsx
@@ -1,2 +1,69 @@
 import React from "react";
-import { Container, Heading, Text } from "@chakra-ui/react";
+import {
+  Container,
+  Heading,
+  Text,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Image,
+  Button,
+  Divider,
+} from "@chakra-ui/react";
+import { supabase } from "../helper/supbaseClient";
+
+export default function ListAllProblems() {
+  const [problems, setProblems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetchProblems();
+  }, []);
+
+  async function fetchProblems() {
+    let { data: problems, error } = await supabase
+      .from("problems")
+      .select("*")
+      .order("created_at", { ascending: false });
+    if (error) console.log("error", error);
+    else setProblems(problems);
+  }
+
+  //fetch image from supabase
+  async function fetchImage(id) {
+    const { data, error } = await supabase.storage
+      .from("public")
+      .download(`images/${id}.jpg`);
+  }
+
+  return (
+    <Container>
+      <Heading mb={5}>List of Problems</Heading>
+      {problems.map((problem) => {
+        console.log(fetchImage(problem.id));
+        return (
+          <Card key={problem.id}>
+            <CardHeader>
+              <Heading as="h3" size="md">
+                {problem.title}
+              </Heading>
+            </CardHeader>
+            <CardBody>
+              <Text>{problem.place}</Text>
+            </CardBody>
+            <CardFooter>
+              <Image
+                src={fetchImage(problem.id)}
+                alt="problem image"
+                boxSize="100px"
+                objectFit="cover"
+                borderRadius="lg"
+                mr={3}
+              />
+            </CardFooter>
+          </Card>
+        );
+      })}
+    </Container>
+  );
+}
