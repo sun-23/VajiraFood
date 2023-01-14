941385df42029b1c6419ac56bb8992d065e8082e
Borwornpob Thumrongchotikhun <borwornpob@gmail.com>
2023-01-14 23:06:27 +0700

    For students part. It's kinda finish ahahha

--- src/routes/ListAllProblems.jsx
+++ src/routes/ListAllProblems.jsx
@@ -15,13 +15,55 @@ import {
 import { supabase } from "../helper/supbaseClient";
 
 export default function ListAllProblems() {
-
-  const status = { "pending": "รอรับเรื่อง", "doing": "กำลังดำเนินการ", "finishing": "เสร็จสิ้น" };
+  const status = {
+    pending: "รอรับเรื่อง",
+    doing: "กำลังดำเนินการ",
+    finishing: "เสร็จสิ้น",
+  };
   const [problems, setProblems] = React.useState([]);
 
   React.useEffect(() => {
     fetchProblems();
   }, []);
+
+  //fucntion parse timestamp to readable date\
+  function parseDate(timestamp) {
+    let date = new Date(timestamp);
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let dt = date.getDate();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    if (dt < 10) {
+      dt = "0" + dt;
+    }
+    if (month < 10) {
+      month = "0" + month;
+    }
+    if (hours < 10) {
+      hours = "0" + hours;
+    }
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+    return (
+      dt +
+      "/" +
+      month +
+      "/" +
+      year +
+      " " +
+      hours +
+      ":" +
+      minutes +
+      ":" +
+      seconds
+    );
+  }
 
   async function fetchProblems() {
     let { data: problems, error } = await supabase
@@ -35,7 +77,7 @@ export default function ListAllProblems() {
   return (
     <Container>
       <Heading mb={5}>List of Problems</Heading>
-      <Stack spacing={4}>
+      <Stack spacing={4} p={3} alignItems="center">
         {problems.map((problem) => {
           return (
             <Card key={problem.id} maxW="sm">
@@ -53,6 +95,8 @@ export default function ListAllProblems() {
                     {problem.title}
                   </Heading>
                   <Text>สถานที่: {problem.place}</Text>
+                  <Text>timestamp: {parseDate(problem.created_at)}</Text>
+                  <Text>ประเภท: {problem.type}</Text>
                 </Stack>
               </CardBody>
               <Divider />
