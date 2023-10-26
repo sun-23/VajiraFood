fe8218fce62e69d5b5caddf7d0004e9e6344fa8e
sun-23 <twinsamson@gmail.com>
2023-10-26 20:48:12 +0700

    c

--- vite.config.js
+++ vite.config.js
@@ -14,25 +14,19 @@ const manifestForPlugin = {
       src: '/android-chrome-192x192.png',
       sizes:'192x192',
       type:'image/png',
-      purpose:'favicon'
+      purpose:'any'
     },
     {
       src:'/android-chrome-512x512.png',
       sizes:'512x512',
       type:'image/png',
-      purpose:'favicon'
+      purpose:'any'
     },
     {
       src: '/apple-touch-icon.png',
       sizes:'180x180',
       type:'image/png',
       purpose:'apple touch icon',
-    },
-    {
-      src: '/maskable_icon.png',
-      sizes:'512x512',
-      type:'image/png',
-      purpose:'any maskable',
     }
   ],
   theme_color:'#186F65',
